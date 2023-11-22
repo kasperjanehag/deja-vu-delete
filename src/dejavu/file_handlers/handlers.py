a6d308d08cf9b1ae0d068a4b10d77663a853f58c
Kasper Janehag <kasper_janehag@mckinsey.com>
2023-11-22 15:26:53 +0100

    Adding md5 hash method

--- src/dejavu/file_handlers/handlers.py
+++ src/dejavu/file_handlers/handlers.py
@@ -9,12 +9,17 @@ class FileHandler(ABC):
     """
     def __init__(self, path):
         self.path = Path(path)
-        self._files = []
+        self._images = []
         self.image_extensions = {".png", ".jpg", ".jpeg", ".gif", ".bmp", ".tiff"}
+        
+        self.average_hash_duplicates = []
+        self.average_hashes = {}
+        self.md5_hashes = {}
+        self.md5_hash_duplicates = []
 
     @property
     @abstractmethod
-    def files(self) -> list[ImageFile]:
+    def images(self) -> list[ImageFile]:
         """ Abstract property to get a list of files. """
         pass
 
@@ -36,13 +41,13 @@ class ImageFileHandler(FileHandler):
                     yield ImageFile(file_path)
 
     @property
-    def files(self) -> list[ImageFile]:
+    def images(self) -> list[ImageFile]:
         """
         Property to get a list of ImageFile instances representing the image files 
         in the instance's directory, done recursively.
 
         Returns:
-            List[ImageFile]: A list of ImageFile instances.
+            list[ImageFile]: A list of ImageFile instances.
 
         Raises:
             ValueError: If the specified path is not a valid directory.
@@ -50,9 +55,56 @@ class ImageFileHandler(FileHandler):
         if not self.path.is_dir():
             raise ValueError(f"{self.path} is not a valid directory.")
 
-        self._files = []  # Reset the list
+        self._images = []  # Reset the list
         for ext in self.image_extensions:
             for file_path in self.path.rglob(f'*{ext}'):
                 if file_path.is_file():
-                    self._files.append(ImageFile(file_path))
-        return self._files
+                    self._images.append(ImageFile(file_path))
+        return self._images
+    
+    def find_duplicates_by_average_hash(self):
+        """
+        Find duplicates based on average hash.
+        """
+        self.average_hash_duplicates = []  # Resetting the duplicates list
+        self.average_hashes = {}  # Resetting the average hash dictionary
+
+        for image in self.images:
+            if image.average_hash in self.average_hashes:
+                self.average_hash_duplicates.append(image)
+            else:
+                self.average_hashes[image.average_hash] = image
+
+        if self.average_hash_duplicates:
+            print(f"Found {len(self.md5_hash_duplicates)} duplicated images based on average hash.")
+
+    def find_duplicates_by_md5_hash(self):
+        """
+        Find duplicates based on MD5 hash.
+        """
+        self.md5_hash_duplicates = []  # Resetting the duplicates list
+        self.md5_hashes = {} # Resetting the MD5 hash dictionary
+
+        for image in self.images:
+            if image.md5_hash in self.md5_hashes:
+                self.md5_hash_duplicates.append(image)
+            else:
+                self.md5_hashes[image.md5_hash] = image
+
+        if self.md5_hash_duplicates:
+            print(f"Found {len(self.md5_hash_duplicates)} duplicated images based on MD5 hash.")
+            
+        #     a = input("Do you want to delete these {} Images? Press Y or N:  ".format(len(duplicates)))
+        #     space_saved = 0
+        #     if(a.strip().lower() == "y"):
+        #         for duplicate in duplicates:
+        #             space_saved += os.path.getsize(os.path.join(self.dirname,duplicate))
+                    
+        #             os.remove(os.path.join(self.dirname,duplicate))
+        #             print("{} Deleted Succesfully!".format(duplicate))
+    
+        #         print("\n\nYou saved {} mb of Space!".format(round(space_saved/1000000),2))
+        #     else:
+        #         print("Thank you for Using Duplicate Remover")
+        # else:
+        #     print("No Duplicates Found :(")
--- src/dejavu/main.py
+++ src/dejavu/main.py
@@ -3,6 +3,6 @@ from dejavu.file_handlers import ImageFileHandler
 if __name__ == "__main__":
 
     path = "/Users/kasper_janehag/Documents/repos/deja-vu-delete/tests/test_set_1"
-    file_handler = ImageFileHandler(path)
-    for image in file_handler.files[0:5]:
-        print(image)
+    image_file_handler = ImageFileHandler(path)
+    image_file_handler.find_duplicates_by_average_hash()
+    image_file_handler.find_duplicates_by_md5_hash()
